6e8ce7e20a04c8266ac530afff79aa1dcb6894f0
PerikiyoXD <perikiyoxd@gmail.com>
2025-01-20 13:37:49 +0100

    chore: Remove precompiled headers and unused SDK files

--- include/SDK/ScriptAPI.hpp
+++ include/SDK/ScriptAPI.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 namespace Universe::Scripting::API
 {
     class IVM;
@@ -9,8 +11,8 @@ namespace Universe::Scripting::API
     class IPropertyCallbackInfo;
 
     using FunctionCallback       = void(ICallbackInfo& info);
-    using AccessorGetterCallback = void(const String& name, IPropertyCallbackInfo& info);
-    using AccessorSetterCallback = void(const String& name, IValue& value, IPropertyCallbackInfo& info);
+    using AccessorGetterCallback = void(const std::string& name, IPropertyCallbackInfo& info);
+    using AccessorSetterCallback = void(const std::string& name, IValue& value, IPropertyCallbackInfo& info);
 
     enum ValueType : char
     {
@@ -32,7 +34,7 @@ namespace Universe::Scripting::API
         virtual bool IsNumber()    = 0;
         virtual bool IsExternal()  = 0;
 
-        virtual String   ToString()   = 0;
+        virtual std::string   ToString()   = 0;
         virtual bool     ToBoolean()  = 0;
         virtual double   ToNumber()   = 0;
         virtual void*    ToExternal() = 0;
@@ -43,29 +45,29 @@ namespace Universe::Scripting::API
     public:
         virtual void* GetInternal() = 0;
 
-        virtual IValue& Get(const String& k) = 0;
+        virtual IValue& Get(const std::string& k) = 0;
 
         virtual void Set(int k, double v)        = 0;
         virtual void Set(int k, int v)           = 0;
         virtual void Set(int k, bool v)          = 0;
-        virtual void Set(int k, const String& v) = 0;
+        virtual void Set(int k, const std::string& v) = 0;
         virtual void Set(int k, IObject& v)      = 0;
         virtual void SetNull(int k)              = 0;
 
-        virtual void Set(const String& k, double v)        = 0;
-        virtual void Set(const String& k, int v)           = 0;
-        virtual void Set(const String& k, bool v)          = 0;
-        virtual void Set(const String& k, const String& v) = 0;
-        virtual void Set(const String& k, IObject& v)      = 0;
-        virtual void SetNull(const String& k)              = 0;
+        virtual void Set(const std::string& k, double v)        = 0;
+        virtual void Set(const std::string& k, int v)           = 0;
+        virtual void Set(const std::string& k, bool v)          = 0;
+        virtual void Set(const std::string& k, const std::string& v) = 0;
+        virtual void Set(const std::string& k, IObject& v)      = 0;
+        virtual void SetNull(const std::string& k)              = 0;
 
-        virtual void SetFunction(const String& k, FunctionCallback callback)                                                                    = 0;
-        virtual void SetAccessor(const String& k, AccessorGetterCallback getterCallback, AccessorSetterCallback setterCallback, char valueType) = 0;
+        virtual void SetFunction(const std::string& k, FunctionCallback callback)                                                                    = 0;
+        virtual void SetAccessor(const std::string& k, AccessorGetterCallback getterCallback, AccessorSetterCallback setterCallback, char valueType) = 0;
     };
 
     class IReturnValue {
     public:
-        virtual void Set(const String& text) = 0;
+        virtual void Set(const std::string& text) = 0;
         virtual void Set(double v)           = 0;
         virtual void Set(int v)              = 0;
         virtual void Set(bool v)             = 0;
@@ -90,22 +92,22 @@ namespace Universe::Scripting::API
         virtual int           Length()                                   = 0;
         virtual IReturnValue& GetReturnValue()                           = 0;
         virtual IObject&      This()                                     = 0;
-        virtual IObject&      ObjectValue(const String& name, void* ptr) = 0;
+        virtual IObject&      ObjectValue(const std::string& name, void* ptr) = 0;
     };
 
     class IGlobal {
     public:
-        virtual void Set(const String& k, double v)        = 0;
-        virtual void Set(const String& k, int v)           = 0;
-        virtual void Set(const String& k, bool v)          = 0;
-        virtual void Set(const String& k, const String& v) = 0;
+        virtual void Set(const std::string& k, double v)        = 0;
+        virtual void Set(const std::string& k, int v)           = 0;
+        virtual void Set(const std::string& k, bool v)          = 0;
+        virtual void Set(const std::string& k, const std::string& v) = 0;
     };
 
     class IVM {
     public:
         virtual IGlobal& Global() = 0;
 
-        virtual void ThrowException(const String& text)                                    = 0;
-        virtual void RegisterGlobalFunction(const String& name, FunctionCallback callback) = 0;
+        virtual void ThrowException(const std::string& text)                                    = 0;
+        virtual void RegisterGlobalFunction(const std::string& name, FunctionCallback callback) = 0;
     };
 } // namespace Universe::Scripting::API
--- include/pch.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef WIN32
-    #define DLLEXPORT extern "C" __declspec(dllexport)
-#else
-    #define DLLEXPORT extern "C" __attribute__((visibility("default")))
-#endif
